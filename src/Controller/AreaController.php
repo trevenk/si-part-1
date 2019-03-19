3e1c3383817e3011f49290a8a4317472347e6d33
Bryan <taylor.bdbi91@gmail.com>
2019-03-19 10:18:01 +0100

    Big problem with AreaController
    I'm going by parts trying to find where the problem is

--- src/Controller/AreaController.php
+++ src/Controller/AreaController.php
@@ -3,39 +3,13 @@
  * Created by PhpStorm.
  * User: trevenk
  * Date: 19/03/19
- * Time: 9:39
+ * Time: 10:15
  */
 
 namespace App\Controller;
 
 
-// ...
-use Symfony\Component\HttpFoundation\Response;
-
-use App\Entity\Area;
-
-class AreaController extends AbstractController
+class AreaController
 {
-    /**
-     * @Route("/area", name="area")
-     */
-    public function index()
-    {
 
-        // traer el entity manager
-        $entityManager = $this->getDoctrine()->getManager();
-
-        // construir e inicializar un area
-        $area = new Area();
-        $area->setNombre('rio');
-        $area->setMesas(5);
-
-        // avisar a Doctrine que quiero CREAR esta Area
-        $entityManager->persist($area);
-
-        // crear el area en la base de datos
-        $entityManager->flush();
-
-        return new Response('Saved new area with id '.$area->getId());
-    }
 }
