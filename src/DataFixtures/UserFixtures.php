dfb539051d701987fb2bd98d9d2f0b6d7a4480e9
Bryan <taylor.bdbi91@gmail.com>
2019-03-21 19:28:16 +0100

    El login funciona en talla
    Cambiar dirección de la página de inicio, para que el log in vaya con todo el tema de seguridad

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,16 +2,32 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
 class UserFixtures extends Fixture
 {
+
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new User();
+        $user->setUsername('Bryan');
+        $user->setPassword($this->passwordEncoder->encodePassword(
+            $user, 'pass1'
+        ));
+        $manager->persist($user);
 
         $manager->flush();
     }
+
+
 }
